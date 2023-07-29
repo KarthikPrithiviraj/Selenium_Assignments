b4ac4532f3bcd4f697edd6d4ca6d4457e46e2773
KarthikPrithiviraj <140888251+KarthikPrithiviraj@users.noreply.github.com>
2023-07-29 16:06:08 +0530

    Add files via upload

---
+++ Week1Day1/MyProfile.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class MyProfile {
+
+	public static void main(String[] args) {
+		String Name = "My name is Karthik";
+		char initial = 'P';
+		int Age = 29;
+		float ht = 5.4f;
+		double salary = 50.55;
+		long MobileNumber = 9597225572l;
+		boolean isinformaionprovidedabove =true;
+		
+		System.out.println(Name+ " " +initial);
+		System.out.println("My Age is" + " " + Age);
+		System.out.println("I'm" + " " + ht + "height");
+		System.out.println("My current Salary is" + "" + salary);
+		System.out.print("And My mobile number is" + "" + MobileNumber);
+		System.out.println("isinformaionprovidedaboveis" + " " + isinformaionprovidedabove );
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Week1Day1/OddorEven.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class OddorEven {
+
+	public static void main(String[] args) {
+		int Number = 15;
+		if (Number/2==0) {
+		System.out.println("The Given Number is Even Number");
+
+	}
+		else if (Number/2!=0) {
+			System.out.println("The Given Number is Odd Number");	
+		}
+
+
+}
+}
