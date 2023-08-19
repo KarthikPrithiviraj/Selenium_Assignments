5e976711af23952af358eb55945720ec9b404367
KARTHIK PRITHIVIRAJ <140888251+KarthikPrithiviraj@users.noreply.github.com>
2023-08-19 18:56:33 +0530

    Add files via upload

---
+++ Week5Day1/BaseClass.java
@@ -0,0 +1,30 @@
+package week5.day1.assignment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseClass {
+	public ChromeDriver driver;
+@BeforeMethod
+	public void PreCondition() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();	
+
+	}
+@AfterMethod
+	public void PostCondition() {
+		driver.close();
+	}
+
+
+}
